9dfc7d32383a0f2757cb2f09d26beeb31cfecb5b
Anisha0906 <anishadas880@gmail.com>
2023-01-05 21:26:49 +0530

    [Anisha]Add:Check alphabet is vowel or consonant

--- Program.cs
+++ Program.cs
@@ -11,7 +11,8 @@ namespace BasicCoreProgramme
             static void Main(string[] args)
             {
                 Console.WriteLine("Welcome to Basic Core Program");
-                 Console.WriteLine("Choose option\n 1.Flip a Coin Head and Tail\t 2.Select Value is Leap Year Or not\t 3.Select Power of 2 Number\t\n 4.Harmonic Number\t 5.Select Factors of number\t 6.Find Quotient and Remainder\t 7.Swap the Numbers\t\n 8.Even and Odd number ");
+                 Console.WriteLine("Choose option\n 1.Flip a Coin Head and Tail\t 2.Select Value is Leap Year Or not\t 3.Select Power of 2 Number\t\n 4.Harmonic Number\t " +
+                     "5.Select Factors of number\t 6.Find Quotient and Remainder\t 7.Swap the Numbers\t\n 8.Even and Odd number\t 9.Select Alphabet Vowel or consonenet ");
                 int selection = Convert.ToInt32(Console.ReadLine());
 
                 switch (selection)
@@ -49,6 +50,10 @@ namespace BasicCoreProgramme
                 case 8:
                     EvenOrOdd EvenOdd = new EvenOrOdd();
                     EvenOdd.CheckEvenOrdd();
+                    break;
+                case 9:
+                    VowelOrNot VowelNot = new VowelOrNot();
+                    VowelNot.CheckGivenVowel();
                     break;
                 default:
                         Console.WriteLine("Invalid Selection.");
---
+++ VowelOrNot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgramme
+{
+    class VowelOrNot
+    {
+        public char Alphabet;
+        public void CheckGivenVowel()
+        {
+            Console.Write("Check whether the input  Alphabet (A-Z or a-z) is a vowel or not:\n");
+            Alphabet = Convert.ToChar(Console.ReadLine());
+            if (Alphabet == 'a' | Alphabet == 'A' | Alphabet == 'i' | Alphabet == 'I' | Alphabet == 'U' | Alphabet == 'u' | Alphabet == 'E' | Alphabet == 'e' | Alphabet == 'O' | Alphabet == 'o')
+                Console.WriteLine("Given" + " " + Alphabet + " is a Vowel");
+            else
+                Console.WriteLine("Given" + " " + Alphabet + " is a Consonant");
+        }
+    }
+}
